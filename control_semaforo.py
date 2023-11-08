2a07b246c3ec84aa3b0b7d13544f8a6764a9f3a7
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2023-11-08 05:34:47 -0500

    Create control_semaforo.py

---
+++ control_semaforo.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+arduino_port = "COMX"  # Reemplaza "COMX" con el puerto COM al que está conectado tu Arduino
+baud_rate = 9600
+
+try:
+    arduino = serial.Serial(arduino_port, baud_rate)
+    print("Conexión establecida con Arduino")
+except:
+    print("No se pudo conectar con Arduino. Verifica el puerto COM.")
+    exit()
+
+while True:
+    command = input("Ingresa 'R' para luz roja, 'Y' para luz amarilla, 'G' para luz verde, o 'Q' para salir: ")
+    if command.upper() == 'Q':
+        break
+    arduino.write(command.encode())
+    time.sleep(0.1)
+
+arduino.close()
